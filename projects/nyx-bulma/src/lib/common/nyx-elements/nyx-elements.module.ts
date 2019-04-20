ad762d7236ca2acb4ffc0f5d301e9771eda88a02
dogukanucak <dogukanucak94@gmail.com>
2019-04-20 21:14:58 +0300

    Add Nyx-Element Module

---
+++ projects/nyx-bulma/src/lib/common/nyx-elements/nyx-elements.module.ts
@@ -0,0 +1,33 @@
+// Nyx-Bulma Elements Module
+// Contributers: @Doğukan UÇAK , @Deniz Yamaç
+
+// Bulma Common Elements - https://bulma.io/documentation/elements/
+
+
+// -------- Development Progress ---------------------------------------------------------
+// Nyx-TextBox - STATUS: Not Started , Exclusive
+// Nyx-Box - STATUS: Not Started
+// Nyx-Button - STATUS: Not Started
+// Nyx-Content - STATUS: Not Started
+// Nyx-Delete - STATUS: Not Started
+// Nyx-Icon - STATUS: Not Started
+// Nyx-Image - STATUS: Not Started
+// Nyx-Notification - STATUS: Not Started
+// Nyx-Progress-Bar - STATUS: Not Started
+// Nyx-Table - STATUS: Not Started
+// Nyx-Tag - STATUS: Not Started
+// Nyx-Title - STATUS: Not Started
+// ----------------------------------------------------------------------------------------
+
+
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class NyxElementsModule { }
