5c05945128bafa3ac983013661fc7360a5e2353d
dogukanucak <dogukanucak94@gmail.com>
2019-06-19 22:56:34 +0300

    Add modal pop up module export

--- projects/nyx-bulma/src/public_api.ts
+++ projects/nyx-bulma/src/public_api.ts
@@ -1,9 +1,12 @@
+
+
 /*
  * Public API Surface of nyx-bulma
  */
 export * from './lib/nyx-bulma.module';
 export * from './lib/common/nyx-elements/nyx-elements.module';
 export * from './lib/common/nyx-layout/nyx-layout.module';
+export * from './lib/common/nyx-components/nyx-modalpopup/nyx-modalpopup.module';
 /*
  * Exported Components
  */
@@ -15,3 +18,8 @@ export * from './lib/common/nyx-layout/nyx-level/nyx-level.component';
 export * from './lib/common/nyx-layout/nyx-level-item/nyx-level-item.component';
 export * from './lib/common/nyx-layout/nyx-level-left/nyx-level-left.component';
 export * from './lib/common/nyx-layout/nyx-level-right/nyx-level-right.component';
+
+/*
+* Exported Services
+*/
+export * from './lib/common/nyx-components/nyx-modalpopup/nyx-modal-pop-up.service';
